6b504a471f98935dffe29eba9c8872c6da444655
Malte Poll <malte.poll@rub.de>
2018-12-11 15:46:03 +0100

    oracle attacker: remove own median calculation

--- src/TestClient/main.cpp
+++ src/TestClient/main.cpp
@@ -20,40 +20,6 @@ namespace po = boost::program_options;
 #define BUFSIZE 1024
 #define SAMPLESIZE 2
 
-void print_array(uint64_t values[], size_t size) {
-    std::cout << "[ " <<std::endl;
-    for (size_t i = 0; i<size; i++) {
-        std::cout << values[i] << ", " << std::endl;
-    }
-    std::cout << " ]" <<std::endl;
-}
-
-uint64_t median(uint64_t values[], size_t size) {
-    if (size == 0) {
-        throw;
-    }
-    if (size == 1) {
-        return values[0];
-    }
-    uint64_t* values_sorted = new uint64_t[size];
-    memcpy(values_sorted, values, size*sizeof(uint64_t));
-    //print_array(values, size);
-    // Sort the values
-    sort(values_sorted, values_sorted+size);
-    //print_array(values_sorted, size);
-    // Pick the middle value
-    uint64_t med;
-    if ((size % 2) != 0) {
-        // odd size -> pick middle
-        med = values_sorted[size/2];
-    }else{
-        // even size -> pick both middle points and calculate average
-        med =  (values_sorted[size/2] + values_sorted[(size/2)-1]) / 2;
-    }
-    //std::cout << med << std::endl;
-    delete[] values_sorted;
-    return med;
-}
 
 int main(int argc, char const *argv[])
 {
@@ -111,42 +77,18 @@ int main(int argc, char const *argv[])
         dynamic_cast<Socket::PCAPTimingSocket*>(ts.get())->initPcap(interface);
     }
     ts->connect(host, port);
-    for (size_t i = 0; i<SAMPLESIZE; i++) {
-        for (size_t j=0; j<measurements; j++) {
+
+    for (size_t j=0; j<measurements; j++) {
+        for (size_t i = 0; i < SAMPLESIZE; i++) {
             ts->write(write_buf, BUFSIZE);
             ssize_t read_size = ts->read(read_buf, BUFSIZE, true);
             times[i].push_back(ts->getLastMeasurement());
             //std::cout << "Measured! "<<times[i][j] << std::endl;
-            if (j==0) {
+            if (j == 0) {
                 correct_case[i] = std::string(read_buf);
             }
         }
-        medians[i] = median(&times[i][0], measurements);
-
-        // cout << "Read " << read_size << " Bytes: " << correct_case[i] << endl;
-        cout << "Median: " << medians[i] << ", " << std::endl;
-        write_buf[0]++;
     }
-    overall_median = median(medians, measurements);
-    cout << "The median is: " << overall_median << endl;
-    size_t success_rate = 0;
-
-    for (size_t i = 0; i<SAMPLESIZE; i++) {
-        std::string guessed_case;
-        if (medians[i] >= overall_median) {
-            guessed_case = "a";
-        }else{
-            guessed_case = "b";
-        }
-        if (guessed_case == correct_case[i]) {
-            cout << "correctly classified case " << correct_case[i] << ":" << medians[i] << endl;
-            success_rate++;
-        }else{
-            cout << "wrongly classified case " << correct_case[i] << " as " << guessed_case << ":" << medians[i] << endl;
-        }
-    }
-
-    cout << "The success rate is " << success_rate << "/" << SAMPLESIZE << " (" << ((double)(100*success_rate) / SAMPLESIZE) << "%)" << endl;
 
     ts->close();
 
